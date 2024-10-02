36d34fa6da32dffe4388795cc64ad87889bc90c0
Liam Stewart <liams@umich.edu>
2024-10-02 14:39:16 -0400

    added state level simulations
    more or less beta at this point, still want to make some adjustments to poll ingestion (aforementioned MC problem) and make a few adjustments to undecided voter allocation

--- Election Simulator.R
+++ Election Simulator.R
@@ -4,6 +4,11 @@ library(tidyverse)
 
 #create districts
 districts <- c(state.name,"District of Columbia", "Maine CD-2", "Maine CD-1", "Nebraska CD-2")
+midwest <- list("Illinois","Indiana","Iowa","Kansas","Michigan","Minnesota","Missouri","Nebraska","North Dakota",
+                "Ohio","Pennsylvania","South Dakota","West Virginia","Wisconsin","Nebraska CD-2")
+southeast <- list("Alabama","Arkansas","Florida","Georgia","Kentucky","Louisiana","Mississippi","North Carolina",
+                  "Oklahoma","South Carolina","Tennessee","Texas","Virginia")
+southwest <- list("Arizona","Colorado","Nevada","New Mexico","Utah")
 
 #pull 538's data if it's been more than 2 hours, otherwise try to load local data, if error then pull 538 data
 if(as.numeric(now()-as.numeric(file.info("timestamp.txt")[4])) > 7200){
@@ -75,6 +80,7 @@ names(stateData) <- c("State")
 stateData$HarrisAvg <- vector(mode = "numeric",length = length(stateData$State))
 stateData$TrumpAvg <- vector(mode = "numeric",length = length(stateData$State))
 stateData$WeightedSampleSize <- vector(mode = "numeric",length = length(stateData$State))
+stateData$TimeDiscountedPollCount <- vector(mode = "numeric",length = length(stateData$State))
 
 #creating a dataframe where each state is a column with total poll results beneath it
 getStateData <- function(state){
@@ -93,11 +99,12 @@ getDataItem <- Vectorize(getDataItem)
 #itereate through each state and calculate a weighted average based on the sqrt of the sample size and inverse of time since poll was released
 for (state in stateData$State){
   ##TODO CLEAN UP AVERAGE CALCULATIONS
-  stateData$HarrisAvg[match(state,stateData$State)] <- 
+  stateData$HarrisAvg[stateData$State == state] <- 
     weighted.mean(filter(pollingDF,District == state)$Harris,sqrt(filter(pollingDF,District == state)$SampleSize*filter(pollingDF,District == state)$TimeDiscount))
-  stateData$TrumpAvg[match(state,stateData$State)] <- 
+  stateData$TrumpAvg[stateData$State == state] <- 
     weighted.mean(filter(pollingDF,District == state)$Trump,sqrt(filter(pollingDF,District == state)$SampleSize*filter(pollingDF,District == state)$TimeDiscount))
-  stateData$WeightedSampleSize[match(state,stateData$State)] <- sum(getDataItem(state,"SampleSize")*getDataItem(state,"TimeDiscount"))
+  stateData$WeightedSampleSize[stateData$State == state] <- sum(getDataItem(state,"SampleSize")*getDataItem(state,"TimeDiscount"))
+  stateData$TimeDiscountedPollCount[stateData$State == state] <- sum(pollingDF$TimeDiscount[pollingDF$District == state])
 }
 stateData$WeightedSampleSize[stateData$WeightedSampleSize<400] <- 400
 
@@ -114,6 +121,7 @@ stateData$HarrisRawWin <- sprintf("%.2f%%",pnorm(stateData$Margin/(stateData$Tru
 writeStateData <- function(write){
   if(tolower(substr(write,1,1)) == "y"){
   write.csv(stateData,file = paste("State_Polling_Data",format(now(),"%Y-%b-%d_%H-%M"),".csv",sep=""))
+  write.csv(ElectionSims,file = paste("Election Simulation Data",format(now(),"%Y-%b-%d_%H-%M"),".csv",sep=""))
   }
 }
 
@@ -134,4 +142,37 @@ ElectionSims$TrumpPopVote <- -1* ElectionSims$USVar * stateData$TrumpError[match
   stateData$Undecided[match("US",stateData$State)]
 ElectionSims$HarrisMargin <- ElectionSims$HarrisPopVote - ElectionSims$TrumpPopVote
 
-writeStateData(readline("Write state polling data file? (Y/N)"))
+#expects a state name, a vector of the popular vote, and a variable for calculating error
+calculateStateMargin <- function(state,popVoteMargin,regionVar){
+  #pull all demo data and polling data needed to make calculations
+  PVI <- DemoData$PVI[DemoData$District == state]
+  HarrisStateAvg <- stateData$HarrisAvg[stateData$State == state]
+  TrumpStateAvg <- stateData$TrumpAvg[stateData$State == state]
+  UndecidedStateAvg <- stateData$Undecided[stateData$State == state]
+  HarrisStateErr <- stateData$HarrisError[stateData$State == state]
+  TrumpStateErr <- stateData$TrumpError[stateData$State == state]
+  PollCount <- stateData$TimeDiscountedPollCount[stateData$State == state]
+  
+  impliedByPVI <- popVoteMargin + PVI
+  HarrisResult <- regionVar * HarrisStateErr + HarrisStateAvg + (pnorm(regionVar/sqrt(2.5))) * UndecidedStateAvg
+  TrumpResult <- -1 * regionVar * TrumpStateErr + TrumpStateAvg + (1-pnorm(regionVar/sqrt(2.5))) * UndecidedStateAvg
+  return(((HarrisResult - TrumpResult) * PollCount + impliedByPVI)/(1+PollCount))
+}
+
+for(state in districts){
+  regionVar <- ElectionSims$USVar
+  if(state %in% midwest){regionVar <- ElectionSims$midwestVar}else if(state %in% southeast){
+    regionVar <- ElectionSims$southeastVar}else if(state%in% southwest){
+      regionVar <- ElectionSims$southwestVar
+    }
+  if(state %in% stateData$State){
+    ElectionSims[,state] <- calculateStateMargin(state,ElectionSims$HarrisMargin,regionVar)
+  }else{
+    ElectionSims[,state] <- regionVar * stateData$HarrisError[stateData$State == "US"] + (2*pnorm(test$USVar/sqrt(2.5))-1) *
+      stateData$Undecided[stateData$State == "US"] + DemoData$PVI[DemoData$District == state]
+  }
+}
+
+ElectionSims$ElectoralVotes <- rowSums(sweep((ElectionSims[,districts]>0),MARGIN = 2,unlist(data.table::transpose(list(DemoData$EVs[DemoData$District == districts]))),`*`))
+
+writeStateData(readline("Write data files? (Y/N)"))
