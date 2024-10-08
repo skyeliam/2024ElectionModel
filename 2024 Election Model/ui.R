9a8e2dc5cc6942015349aedb089f86bb4040a6d7
Liam Stewart <liams@umich.edu>
2024-10-07 21:14:32 -0400

    Heroku Deplloyment Files

--- 2024 Election Model/ui.R
+++ 2024 Election Model/ui.R
@@ -2,8 +2,12 @@
 library(shiny)
 library(tidyverse)
 library(shinydashboard)
+library(DescTools)
 
-
+latestSimData <- read.csv(url("https://raw.githubusercontent.com/skyeliam/2024ElectionModel/refs/heads/main/Latest_Election%20Simulation%20Data.csv"))
+colnames(latestSimData) <- str_replace_all(colnames(latestSimData),"\\."," ")
+timeStamp <- StrExtractBetween(read_file(url(
+  "https://raw.githubusercontent.com/skyeliam/2024ElectionModel/refs/heads/main/timestamp.txt"))," ","\n")
 #for the drop down menu to get state level data
 stateOptions <- c(state.name,"Maine CD 2","Maine CD 2","Nebraska CD 2")
 
